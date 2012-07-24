9bf855994da24059e3187691d120a641951d7077
Andrei Gigirtu <andrei@thinkcentre.sinapticode.ro>
2012-07-24 15:09:38 +0300

    login flow

--- app/code/local/Wonderhop/Sales/controllers/IndexController.php
+++ app/code/local/Wonderhop/Sales/controllers/IndexController.php
@@ -4,5 +4,131 @@
 		$this->loadLayout(array('default'));
 		$this->renderLayout();
     }
+    
+    
+    public function mailcheckerAction()
+    {
+
+        if( $this->getRequest()->isPost() ) {
+            $post  = $this->getRequest()->getPost();
+            if( $post) {
+                $mail_to_check = $this->getRequest()->getParam('email') ;
+
+                    $customers = Mage::getModel('customer/customer')->getCollection();
+                $customers->addAttributeToFilter( 'email', $mail_to_check );
+                $customers->load();
+
+                if ($customers->count()) {
+                            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array('ERR')));
+                        } else {
+                            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array('OK')));
+                    }
+                } else {
+                        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array('ERR')));
+                }
+        } else {
+            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array('ERR')));
+        }
+    }
+    
+    
+    /**
+     * Define target URL and redirect customer after logging in
+     */
+    protected function _loginPostRedirect()
+    {
+        $session = $this->_getSession();
+
+        if (!$session->getBeforeAuthUrl() || $session->getBeforeAuthUrl() == Mage::getBaseUrl()) {
+            // Set default URL to redirect customer to
+            $session->setBeforeAuthUrl(Mage::helper('customer')->getAccountUrl());
+            // Redirect customer to the last page visited after logging in
+            if ($session->isLoggedIn()) {
+                if (!Mage::getStoreConfigFlag(
+                    Mage_Customer_Helper_Data::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD
+                )) {
+                    $referer = $this->getRequest()->getParam(Mage_Customer_Helper_Data::REFERER_QUERY_PARAM_NAME);
+                    if ($referer) {
+                        // Rebuild referer URL to handle the case when SID was changed
+                        $referer = Mage::getModel('core/url')
+                            ->getRebuiltUrl(Mage::helper('core')->urlDecode($referer));
+                        if ($this->_isUrlInternal($referer)) {
+                            $session->setBeforeAuthUrl($referer);
+                        }
+                    }
+                } else if ($session->getAfterAuthUrl()) {
+                    $session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
+                }
+            } else {
+                $session->setBeforeAuthUrl(Mage::helper('customer')->getLoginUrl());
+            }
+        } else if ($session->getBeforeAuthUrl() == Mage::helper('customer')->getLogoutUrl()) {
+            $session->setBeforeAuthUrl(Mage::helper('customer')->getDashboardUrl());
+        } else {
+            if (!$session->getAfterAuthUrl()) {
+                $session->setAfterAuthUrl($session->getBeforeAuthUrl());
+            }
+            if ($session->isLoggedIn()) {
+                $session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
+            }
+        }
+        $this->_redirectUrl($session->getBeforeAuthUrl(true));
+    }
+    
+    /**
+     * Retrieve customer session model object
+     *
+     * @return Mage_Customer_Model_Session
+     */
+    protected function _getSession()
+    {
+        return Mage::getSingleton('customer/session');
+    }
+    
+     /**
+     * Login post action
+     */
+    public function loginPostAction()
+    {
+        if ($this->_getSession()->isLoggedIn()) {
+            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array('/sales')));
+            return;
+        }
+        $session = $this->_getSession();
+
+        if ($this->getRequest()->isPost()) {
+           
+            $login = $this->getRequest()->getPost('login');
+            if (!empty($login['username']) && !empty($login['password'])) {
+                try {
+                    $session->login($login['username'], $login['password']);
+                    $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array('/sales')));
+                    
+                    return;
+                } catch (Mage_Core_Exception $e) {
+                    switch ($e->getCode()) {
+                        case Mage_Customer_Model_Customer::EXCEPTION_EMAIL_NOT_CONFIRMED:
+                            $value = Mage::helper('customer')->getEmailConfirmationUrl($login['username']);
+                            $message = Mage::helper('customer')->__('This account is not confirmed. <a href="%s">Click here</a> to resend confirmation email.', $value);
+                            break;
+                        case Mage_Customer_Model_Customer::EXCEPTION_INVALID_EMAIL_OR_PASSWORD:
+                            $message = $e->getMessage();
+                            break;
+                        default:
+                            $message = $e->getMessage();
+                    }
+                    $session->addError($message);
+                    $session->setUsername($login['username']);
+                } catch (Exception $e) {
+                    // Mage::logException($e); // PA DSS violation: this exception log can disclose customer password
+                }
+            } else {
+                $session->addError($this->__('Login and password are required.'));
+            }
+        }
+
+        $this->_loginPostRedirect();
+    }
+
  
 }
---
+++ skin/frontend/wonderhop/default/js/sales/login_popup.js
@@ -0,0 +1,34 @@
+function buildBlock(element) {
+    jQuery.blockUI({ 
+            message: jQuery(element), 
+            // styles for the message when blocking; if you wish to disable 
+            // these and use an external stylesheet then do this in your code: 
+            // $.blockUI.defaults.css = {}; 
+            css: { 
+                padding:        '20px', 
+                margin:         0, 
+                width:          '30%', 
+                top:            '30%', 
+                left:           '35%', 
+                textAlign:      'center', 
+                color:          '#000', 
+                border:         '3px solid #aaa', 
+                backgroundColor:'#fff', 
+                cursor:         'auto'
+            }, 
+            // styles for the overlay 
+            overlayCSS:  { 
+                backgroundColor: '#000', 
+                opacity:         0.6 ,
+                cursor:         'auto' 
+            }, 
+        });
+}
+jQuery(document).ready(function() { 
+     buildBlock('.popup_register'); 
+     
+      
+     jQuery('#login_link').click(function(){
+         buildBlock('.popup_login'); 
+     }); 
+}); 
