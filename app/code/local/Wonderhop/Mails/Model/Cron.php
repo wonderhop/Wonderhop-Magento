8749c1ab8ba8144c3b06bb7348a93652cd744057
Andrei Gigirtu <andrei@thinkcentre.sinapticode.ro>
2012-08-06 15:18:32 +0300

    set time

--- app/code/local/Wonderhop/Mails/Model/Cron.php
+++ app/code/local/Wonderhop/Mails/Model/Cron.php
@@ -18,21 +18,27 @@ class Wonderhop_Mails_Model_Cron {
     
   	function executeCron () {
 	
-	    $this->getLogger()->log("xxxx");    
+	  
+	    $date = new DateTime(date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time())));
 	    $active =  Mage::getStoreConfig('Wonderhop_Sales/general/sales_newsletter_active',Mage::app()->getStore());
 	    
 	    if (!$active) {
+	        return;
+	    }
+	    list ($run_hour, $run_min, $x)  = explode(',', Mage::getStoreConfig('Wonderhop_Sales/general/daily_newsletter_run_time', Mage::app()->getStore()));
+	    
+        if (($date->format('H') != $run_hour || $date->format('i') != $run_min))  {
 	        return;
 	    }
 	    
 	    $templateId = Mage::getStoreConfig('Wonderhop_Sales/general/daily_newsletter_template',Mage::app()->getStore()); 
 	    
 	    $customers = Mage::getModel('customer/customer')->getCollection()->load();
-	    #foreach($customers as $customer) {
-        #    $customer = Mage::getModel('customer/customer')->load($customer->getId());
-        #}
-        $customer = Mage::getModel('customer/customer')->load(18);
-        $this->sendTransactionaEmail($templateId, $customer);
+	    foreach($customers as $customer) {
+            $customer = Mage::getModel('customer/customer')->load($customer->getId());
+             $this->sendTransactionaEmail($templateId, $customer);
+        }
+     
     } 
     
 	public function sendTransactionaEmail($templateId, $customer, $comment = NULL, $custom_vars = NULL) {
