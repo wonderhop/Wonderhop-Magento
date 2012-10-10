d024954bc9f9a576ed0e885a8f99af9f81d0fec4
Andrei Gigirtu <andrei@thinkcentre.sinapticode.ro>
2012-10-10 17:49:26 +0300

    Mailchimp module

--- app/code/local/Wonderhop/Mails/Model/Cron.php
+++ app/code/local/Wonderhop/Mails/Model/Cron.php
@@ -1,5 +1,33 @@
 <?php 
-
+ chdir("/var/www/Wonderhop-Magento/");
+require 'app/Mage.php';
+if (!Mage::isInstalled()) {
+    // We cannot run the Magento instance if it isn't installed.
+    echo "Application is not installed yet, please complete install wizard first.";
+    exit;
+}
+// Here you should put unique identifier of store view / store group / website,
+// if you leave this field empty, application will use the default value
+$initializationCode = 'admin';
+// Means that Magento will be initialized on a specified store view
+// Also you can use such types like "website" or "group"
+// for initialization on specific website or store group
+$initializationType = 'store';
+// Specifies the scope of you application.
+// Magento has three scopes that used in core: "adminhtml", "fontend" and "crontab",
+// but you can create your own
+$scope = 'frontend';
+// Initialize Mage_Core_Model_App object
+Mage::app($initializationCode, $initializationType);
+// Load configuration
+Mage::getConfig()->init();
+// Load event observers for specified scope
+Mage::getConfig()->loadEventObservers($scope);
+// Add event area for event dispatching
+Mage::app()->addEventArea($scope);
+ 
+ $x = new Wonderhop_Mails_Model_Cron();
+ $x->executeCron();
 class Wonderhop_Mails_Model_Cron {
     
     public $logger; 
@@ -32,8 +60,27 @@ class Wonderhop_Mails_Model_Cron {
 	        return;
 	    }
 	     
+	     return;
 	    $templateId = Mage::getStoreConfig('Wonderhop_Sales/general/daily_newsletter_template',Mage::app()->getStore()); 
-	    
+	    //Ebizmarts_MageMonkey_Model_Api
+	    $template = Mage::getModel('core/email_template');
+	    $template->load($templateId);
+        $templateProcessed = $template->getProcessedTemplate(array(), true);
+        
+        $type = 'regular';
+        $opts['list_id'] = '236ee2fdd6';
+        $opts['subject'] = 'Test Newsletter Subject';
+        $opts['from_email'] = 'andrei@sinapticode.ro'; 
+        $opts['from_name']  = 'ACME, Inc.';
+        $opts['tracking']   = array('opens' => true, 'html_clicks' => true, 'text_clicks' => false);
+        $opts['title'] = 'Test Newsletter Title';
+        $content = array('html'=> $templateProcessed, 
+		  'text' => 'text text text *|UNSUB|*'
+		);
+        
+        Mage::getSingleton('monkey/api', array('store' => 1))
+									->campaignCreate($type, $opts, $content);
+		return;							
 	    $customers = Mage::getModel('customer/customer')->getCollection()->load();
 	    foreach($customers as $customer) {
             $customer = Mage::getModel('customer/customer')->load($customer->getId());
